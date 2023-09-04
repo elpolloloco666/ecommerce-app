c4ffff4f99856cd5f2045e4d2d4d977625d23b39
ENRIQUE ACOSTA RIVERA <enriqueacostarivera@comunidad.unam.mx>
2023-09-04 17:32:27 -0600

    the scrollbar was hidden and some slices that were not used in the redux store were removed

--- redux/store.js
+++ redux/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import customerSlice from './customerSlice';
 import cartSlice from './cartSlice';
 import toastSlice from './toastSlice';
 
 const store = configureStore({
     reducer:{
-        customer: customerSlice.reducer,
         cart: cartSlice.reducer,
         toast: toastSlice.reducer
     }
